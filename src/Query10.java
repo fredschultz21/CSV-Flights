7998dcaf888e6002d0c485d25b6baee31c847a5e
fredschultz21 <schultzfred8@gmail.com>
2025-08-21 21:07:28 -0500

    Update Query10.java

--- src/Query10.java
+++ src/Query10.java
@@ -1,9 +1,3 @@
-// DO NOT REMOVE PACKAGE LINE
-// Without this line the autograder will not run correctly
-// You can comment it while you work on the problem
-// When everything works - uncomment and submit!
-package com.gradescope.hw;
-
 import java.io.IOException;
 import java.util.*;
 
